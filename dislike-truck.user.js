8d5617282cff0de09638e6b3ea585ff21154ed42
Seizure Salad <67077815+SeizureSaladd@users.noreply.github.com>
2021-05-17 20:12:54 -0400

    Update dislike-truck.user.js

--- dislike-truck.user.js
+++ dislike-truck.user.js
@@ -19,8 +19,8 @@
 
 $(function() {
 
-  var CLIENT_ID = '595110168346-46igp17sotrer74ld1rbg4onc5smse60.apps.googleusercontent.com',
-      API_KEY = 'AIzaSyByJQv-QxraMe7iNFEszkcnNk8JfPRTljY',
+  var CLIENT_ID = '427381992847-tvqbbdub3sb6ps3crifghuha9i32r4qi.apps.googleusercontent.com',
+      API_KEY = 'KJiURDrLxW4o6UYy2daiERky',
       SCOPES = 'https://www.googleapis.com/auth/youtube',
       PER_PAGE = 50,
       DELAY_TIME = 500,
